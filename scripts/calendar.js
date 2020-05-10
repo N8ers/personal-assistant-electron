155c67cdfe3313fcaf9926e1459afb5f60226b54
nathan sheryak <nathanraysheryak@gmail.com>
2020-05-10 10:35:13 -0400

    get calendar params working

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,43 +1,60 @@
 const axios = require('axios');
 const moment = require('moment')
 
-let CLIENT_ID = '452597098857-mdep05i3si61cgbqv210ti253pld6p1o.apps.googleusercontent.com';
-let API_KEY = 'Q3sk61C_BKtEWwg0RTGlcq3Z';
+let now = (new Date()).toISOString()
+console.log('now: ', now)
+
+let eod = (new Date()).setHours(23,59,59)
+console.log('eod: ', eod)
+
+let newEOD = new Date(eod).toISOString()
+console.log('newEOD: ', newEOD)
 
 let optionalArgs = {
   timeMin: (new Date()).toISOString(),
+  timeMax: newEOD,
   showDeleted: false,
   singleEvents: true,
-  maxResults: 10,
+  // maxResults: 2,
   orderBy: 'startTime'
 };
 
 function makeTable (data) {
-  data.forEach(event => {
+  if (data.length > 0) {
+    console.log('if block fired')
+    data.forEach(event => {
+      let div = document.createElement("div");
+      document.getElementById("calendarEvents").appendChild(div);
+      div.innerHTML += `
+        <table class="event">
+          <td class="first-td">
+            ${moment(event.start.dateTime).format('h:mm a')}
+            </br>
+            ${moment(event.end.dateTime).format('h:mm a')}
+          </td>
+          <td class="second-td">${event.summary}</td>
+        </table>
+      `
+    })
+  } else {    
+    console.log('else block fired')
     let div = document.createElement("div");
     document.getElementById("calendarEvents").appendChild(div);
-    div.innerHTML += `
-      <table class="event">
-        <td class="first-td">
-          ${moment(event.start.dateTime).format('h:mm a')}
-          </br>
-          ${moment(event.end.dateTime).format('h:mm a')}
-        </td>
-        <td class="second-td">${event.summary}</td>
-      </table>
-    `
-  })
+    div.innerHTML += `Looks like you're free today!`
+  }
 }
 
-async function getCalendar(_, optionalArgs) {
-  const PUBLIC_KEY = 'AIzaSyA3Vj8opTojQsgHvRR7WuugqdTUDb8xJsI',
-  CALENDAR_ID = '983grotkd897333tqiuq4hj8t0@group.calendar.google.com';
+async function getCalendar(optArgs) {
+  const publicKey = 'AIzaSyA3Vj8opTojQsgHvRR7WuugqdTUDb8xJsI',
+  calendarId = '983grotkd897333tqiuq4hj8t0@group.calendar.google.com';
   const dataUrl = [ 'https://www.googleapis.com/calendar/v3/calendars/',
-  CALENDAR_ID, '/events?key=', PUBLIC_KEY].join('');
+  calendarId, '/events?key=', publicKey].join('');
 
-  return fetch(dataUrl, optionalArgs).then(
-    (response) => response.json()
-  ).then((data) => makeTable(data.items));
+  let getEvents = await axios.get(dataUrl, {
+    params: optionalArgs
+  })
+
+  makeTable(getEvents.data.items)
 }
 
 getCalendar();
